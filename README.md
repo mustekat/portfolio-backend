c741b926acda9b0250374b724d6c47dec506cc71
mustekat <>
2018-08-05 10:26:42 +0300

    App route structure, first request and env vars

--- README.md
+++ README.md
@@ -5,11 +5,17 @@ This is a simple backend designed to run in Google App Engine for a portfolio we
 ## Start developing
 Install [Google Cloud Tools](https://cloud.google.com/sdk/docs/).
 
+Copy `.env.sample`into `.env` and fill with your values used in development. Similarly, copy `app.sample.yaml` into `app.yaml`and fill with values used in production. The App Engine will automatically set some values, e.g. `NODE_ENV` as `production` and `PORT` as `8080`;
+
+    source .env
+
     yarn install
 
 ## Development
 
     yarn start
+
+Go to [localhost:8080](localhost:8080) if you are using the default port.
 
 ## Deploying to App Engine
 
--- app.js
+++ app.js
@@ -1,14 +1,53 @@
-'use strict';
-
 const express = require('express');
+const request = require('request');
 
+const router = express.Router();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
-});
+const APP_URL = process.env.APP_URL;
+const FOLDER_NAME = process.env.FOLDER_NAME;
 
-const PORT = process.env.PORT || 8000;
+const storageUrl = `https://storage.googleapis.com/${APP_URL}/${FOLDER_NAME}`;
+const imageListUrl = `${storageUrl}/image-list.txt`;
+const imageUrl = fileName => `${storageUrl}/${fileName}`;
+
+const getList = (req, res, next) => {
+  request(imageListUrl, (error, response, body) => {
+    const statusCode = response && response.statusCode;
+    if (statusCode === 200) {
+      const imageList = body.split('\n');
+      res.status(200).json({ result: imageList });
+    } else {
+      console.error('Error in getList', statusCode, error, body);
+      res.status(statusCode || 500).json({ error, body });
+    }
+  });
+};
+
+const updateList = (req, res, next) => {
+  res.status(200).send('Dummy response for update list');
+};
+
+const addImage = (req, res, next) => {
+  res.status(200).send('Dummy response for add image');
+};
+
+const deleteImage = (req, res, next, id) => {
+  res.status(200).send(`Dummy response for delete ${id}`);
+};
+
+router.route('/images').post(addImage);
+
+router.route('/images/:imageId').delete(deleteImage);
+
+router
+  .route('/image-list')
+  .get(getList)
+  .post(updateList);
+
+app.use('/api/v1', router);
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
